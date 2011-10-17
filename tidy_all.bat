62da96cdab3f591249fbb6e4330ccc583dfcde06
Amalia Nεrdstrφm <devnull@localhost>
2011-10-17 23:32:46 +0300

    New batch script

---
+++ tidy_all.bat
@@ -0,0 +1,5 @@
+java -jar FileJanitor.jar --tidy . & ^
+java -jar FileJanitor.jar --tidy ..\ankh\Images & ^
+java -jar FileJanitor.jar --tidy ..\ankh\Textures& ^
+java -jar FileJanitor.jar --tidy ..\ankh\NURBS & ^
+pause
