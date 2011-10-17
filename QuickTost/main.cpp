61fbd7f0e67251bd31e1d1c7a5b77b79cf4e1c9c
Chopione Granger <devnull@localhost>
2011-10-17 19:52:38 +0300

    Added a stand-alone tost for exhibiting AO computers incosistencies

--- QuickTost/main.cpp
+++ QuickTost/main.cpp
@@ -1,4 +1,4 @@
-static size_t const RunMain(		4		);
+static size_t const RunMain(		5		);
 
 #define DECLARE_MAIN(N) \
 	extern int main##N (int, char**);
@@ -10,13 +10,15 @@ DECLARE_MAIN(1)
 DECLARE_MAIN(2)
 DECLARE_MAIN(3)
 DECLARE_MAIN(4)
+DECLARE_MAIN(5)
 
 static int (*const mains[]) (int, char**) = {
 	ADDRESSOF_MAIN(0),
 	ADDRESSOF_MAIN(1),
 	ADDRESSOF_MAIN(2),
 	ADDRESSOF_MAIN(3),
-	ADDRESSOF_MAIN(4)
+	ADDRESSOF_MAIN(4),
+	ADDRESSOF_MAIN(5)
 };
 
 int main (int const argc, char* argv[]) {
