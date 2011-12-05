29ed849d621e663e58463cffaec40eb57e5e603f
Chopione Granger <devnull@localhost>
2011-12-05 15:24:09 +0200

    Added RequiredOpenGlExtensions.txt here.

--- glext_generator.bat
+++ glext_generator.bat
@@ -1,5 +1,5 @@
 ruby glext_generator.rb ^
 	"C:\Program Files\Microsoft SDKs\Windows\v7.0A\Include\gl\GL.h" ^
 	MyUtil\Include\gl\glext.h ^
-	..\ankh\Images\Include\nmutil\OpenGlExtensions.txt ^
+	RequiredOpenGlExtensions.txt ^
 	..\ankh\Images\Include\nmutil\OpenGlExtensions.h
