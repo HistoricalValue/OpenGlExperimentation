1013d7f1afb3167e6b1206b3c56f6ea3bb0e2694
Chopione Granger <devnull@localhost>
2011-12-05 14:06:10 +0200

    UNIX format txt files too. GL-ext generator turns super.

--- glext_generator.rb
+++ glext_generator.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 header = '// OpenGlExtensions.h
 // OpenGL extension enums and functions.
 //
@@ -38,15 +40,41 @@ typedefs << ['ptrdiff_t'		, 'GLintptr'	]
 typedefs << ['ptrdiff_t'		, 'GLsizeiptr'	]
 typedefs << ['struct __GLsync*'	, 'GLsync'		]
 
+class IterableConcatenation
+	def initialize (*iterables)
+		@iterables = iterables
+	end
+	
+	def each (&b)
+		iterables = @iterables.dup
+		until iterables.empty?
+			iterable = iterables.shift
+			iterable.each(&b)
+		end
+	end
+end
+
 def enums_to_s (enums)
-	enums.sort! { |a, b| a.last <=> b.last }
+	enums[:num].sort! { |a, b|
+		if (cmp = a.last <=> b.last) != 0
+			cmp
+		else
+			a.first <=> b.first
+		end
+	}
+	enums[:str].sort! { |a, b| a.first <=> a.first }
+
 	maxname = 0
-	for enum_pair in enums
+	for enum_pair in IterableConcatenation.new(enums[:num], enums[:str])
 		maxname = enum_pair.first.length if enum_pair.first.length > maxname
 	end
+
 	lines = []
-	for enum_pair in enums
+	for enum_pair in enums[:num]
 		lines << sprintf('#define %-*s %#06x', maxname, enum_pair.first, enum_pair.last)
+	end
+	for enum_pair in enums[:str]
+		lines << sprintf('#define %-*s %s', maxname, enum_pair.first, enum_pair.last)
 	end
 	lines.join("\n")
 end
@@ -64,23 +92,70 @@ def typedefs_to_s (typedefs)
 	lines.join("\n")
 end
 
-def parse_gl_ext (fin)
-	enums = []
+def parse_gl_ext (fin, requested)
+	enums = { :num => [], :str => [] }
+	
 	fin.each_line { |line|
-		line.chomp!
+		line.strip!
 		case
 			when line.empty?
 			when md = /^\#define\s+([\w_]+)\s+([^\s]+)\s*$/.match(line)
 				name = md[1]
-				val = Integer(md[2])
-				enums << [name, val]
+				begin
+					val = Integer(md[2])
+					append_to = :num
+				rescue ArgumentError => e
+					val = md[2]
+					append_to = :str
+				end
+				
+				if requested.has_key?(name) then	
+					enums[append_to] << [name, val]
+					requested.delete(name)
+				end
 		end
 	}
+	
+	raise "shithell, not all requested requests met: #{requested.size}: #{requested.inspect}" unless requested.empty?
+
 	enums
 end
 
-File.open(ARGV[1], IO::CREAT | IO::TRUNC | IO::WRONLY) { |fout|
-	File.open(ARGV.first, IO::RDONLY) { |fin|
-		fout.puts header, enums_to_s(parse_gl_ext(fin)), typedefs_to_s(typedefs), footer
+def parse_requested_exts (fin)
+	requested = {}
+	fin.each_line { |line|
+		line.strip!
+		case
+			when line.empty?
+			when requested.has_key?(line)
+				raise "hell: #{line.inspect}"
+			when line =~ /^\/\//
+			else
+				requested[line] = true
+		end
 	}
+	requested 
+end
+
+class StreamConcatenation
+	def initialize (*streams)
+		@streams = streams
+	end
+	
+	def each_line (&b)
+		streams = @streams.dup
+		until streams.empty?
+			stream = streams.shift
+			stream.each_line(&b)
+		end
+	end
+	
+end
+
+File.open(ARGV[3], IO::CREAT | IO::TRUNC | IO::WRONLY) { |fout|
+	File.open(ARGV[0], IO::RDONLY) { |fin_gl|
+	File.open(ARGV[1], IO::RDONLY) { |fin_glext|
+	File.open(ARGV[2], IO::RDONLY) { |fin_requested|
+		fout.puts header, enums_to_s(parse_gl_ext(StreamConcatenation.new(fin_gl, fin_glext), parse_requested_exts(fin_requested))), typedefs_to_s(typedefs), footer
+	}}}
 }
