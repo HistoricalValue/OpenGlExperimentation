6b9fb626c10d418496f9ce49a621bb5a8b82a823
Chopione Granger <devnull@localhost>
2011-12-15 12:09:42 +0200

    Tidying in two new projects.

--- tidy_all.bat
+++ tidy_all.bat
@@ -2,5 +2,7 @@ java -jar FileJanitor.jar --tidy . & ^
 java -jar FileJanitor.jar --tidy ..\ankh\Images & ^
 java -jar FileJanitor.jar --tidy ..\ankh\Textures& ^
 java -jar FileJanitor.jar --tidy ..\ankh\NURBS & ^
+java -jar FileJanitor.jar --tidy ..\ankh\NURBSx & ^
 java -jar FileJanitor.jar --tidy ..\ankh\Bumps & ^
+java -jar FileJanitor.jar --tidy ..\ankh\Parallelisation & ^
 pause
